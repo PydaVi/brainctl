40ff36e5171ddf56737e25fda0fdf1075a405fc9
Fabio Souza <132482742+PydaVi@users.noreply.github.com>
2026-02-22 18:51:27 -0300

    Pin AWS provider in generated root Terraform config

--- internal/generator/ec2_app.go
+++ internal/generator/ec2_app.go
@@ -17,6 +17,14 @@ import (
 const mainTF = `
 terraform {
   required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
   backend "s3" {
     bucket       = "{{ .Terraform.Backend.Bucket }}"
     key          = "{{ .BackendKey }}"
